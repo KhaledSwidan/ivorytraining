d130a62623a942f35b0e7dfac65cd12d9a9f98ab
khaled swidan <khaledqutb4@gmail.com>
2025-02-17 20:07:18 +0200

    Resolved merge conflicts

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 import LanguageSwitcher from './language-switcher';
 import { Routes, UserRole } from '../../constants/enums';
+import { Translations } from '../../constants/links';
 
 interface NavbarProps {
   translations: Translations;
